aec0cb3709dd9580d120a8f84cb335906c3b9302
Anže Jenšterle <anze.jensterle@castawaylabs.com>
2015-07-01 09:42:48 +0100

    IC

---
+++ generate.js
@@ -0,0 +1,121 @@
+var jade = require('jade');
+
+var sections = [
+  {
+    name: 'Hot Tapas',
+    items: [
+      {
+        name: 'Patates bravas',
+        description: 'Diced potates with spicy chilli tomato sause & aioli',
+        attrs: ['v', 'g']
+      }, {
+        name: 'Tortilla de patatas',
+        description: 'Spanish omelette made with potato & onion',
+        attrs: ['v', 'g']
+      }, {
+        name: 'Champinones al ajiool',
+        description: 'Sautéed button mushrooms with garlic and parsley',
+        attrs: ['v']
+      }, {
+        name: 'Fritos de queso de cabra a la miel',
+        description: 'Deep fried medallions of goats cheese with honey',
+        attrs: ['v']
+      }, {
+        name: 'Chorizos de la casa',
+        description: 'Oven baked spicy Spanish sausage',
+        attrs: ['g']
+      }, {
+        name: 'Lentejas',
+        description: 'Slow cooked Spanish brown lentils with chorizo',
+        attrs: ['g']
+      }, {
+        name: 'Albondigas',
+        description: 'Homemade Pork & Beef meatballs in tomato sauce '
+      }, {
+        name: 'Pollo con crema y piquillos',
+        description: 'Small pieces of chicken breast cooked in cream and piquillo pepper sauce',
+        attrs: ['g']
+      }, {
+        name: 'Esparragos a la plancha ',
+        description: 'Grilled wild asparagus wrapped with Serrano ham',
+        attrs: ['g']
+      }, {
+        name: 'Pollo y chorizo con patatas',
+        description: 'Small pieces of chicken, chorizo, potatoes sautéed with onion, pepper, white w',
+        attrs: ['g']
+      }, {
+        name: 'Milhogas de queso de cabra',
+        description: 'Oven baked potato layered with Serrano ham, goats cheese, tomato & bread'
+      }, {
+        name: 'Datiles con bacon',
+        description: 'Dates wrapped with bacon and filled with blue cheese. Grilled & served warm',
+        attrs: ['g']
+      }, {
+        name: 'Pinchos de cordero al limón ',
+        description: 'Pieces of lamb marinated with lemon juice & cumin, grilled on skewer'
+      }, {
+        name: 'Calamares a la romana ',
+        description: 'Deep fried squid '
+      }, {
+        name: 'Pulpo a la Gallega',
+        description: 'Chopped octopus legs served with boiled potatoes topped with olive oil, paprika ',
+        attrs: ['g']
+      }, {
+        name: 'Mejillones',
+        description: 'Fresh mussels with garlic, white wine, parsley & cream'
+      }, {
+        name: 'Gambas a la plancha ',
+        description: 'Grilled black tiger prawns served with aioli',
+        attrs: ['g']
+      }, {
+        name: 'Gambas al Pil-Pil ',
+        description: 'Tiger prawns pan-fried with garlic & chilli oil ',
+        attrs: ['g']
+      }, {
+        name: 'Paella de verduras ',
+        description: 'Vegetarian paella of fresh seasonal vegetables',
+        attrs: ['v', 'g']
+      }, {
+        name: 'Paella Espanola ',
+        description: 'Traditional Paella with chicken & chorizo'
+      }, {
+        name: 'Paella de marisco',
+        description: 'Traditional Mixed Seafood paella ',
+        attrs: ['g']
+      }, {
+        name: 'Boguerones en adobo',
+        description: 'Marinated crispy whitebait'
+      }, {
+        name: 'Bunuelos de bacalao',
+        description: 'Light and fluffy cod fritters with garlic, parsley and aioli'
+      }, {
+        name: 'Crocetas de la casa',
+        description: 'Ham crocetas'
+      }
+    ]
+  }, {
+    name: 'Cold Tapas',
+    items: [
+      {
+        name: 'Aceitunas',
+        description: 'Marinated olives'
+      }, {
+        name: 'Boquerones en vinagre',
+        description: 'White marinated anchovies in vinegar and olive oil',
+        attrs: ['g']
+      }, {
+        name: 'Pimientos del piquillo',
+        description: 'Sweet piquillo peppers in olive oil and garlic dressing.',
+        attrs: ['v', 'g']
+      },
+    ]
+  }
+];
+
+var html = jade.compileFile('index.jade', {
+  pretty: true
+})({
+  sections: sections
+});
+
+require('fs').writeFileSync(__dirname + '/generated.html', html);

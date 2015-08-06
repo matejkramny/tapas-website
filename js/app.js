339a427a2ab4938f0cadf6c36b8bd8c461964751
Matej Kramny <matejkramny@gmail.com>
2015-08-07 00:10:14 +0100

    Angular stuff, basket working, etc.

---
+++ js/app.js
@@ -0,0 +1,142 @@
+var app = angular.module('tapas', ['LocalStorageModule']);
+
+app.config(function (localStorageServiceProvider) {
+  localStorageServiceProvider.setPrefix('tapas');
+});
+
+app.run(function ($rootScope, basketService) {
+	$rootScope.basket = basketService;
+});
+
+app.controller('MenuCtrl', function ($scope, basketService) {
+	$scope.addToBasket = function (id) {
+		basketService.addItem(id);
+	}
+});
+
+app.controller('BasketCtrl', function ($scope, $http, basketService, localStorageService) {
+	try {
+		$scope.customer = JSON.parse(localStorageService.get('customer'));
+		if (!$scope.customer) throw Error();
+	} catch (e) {
+		$scope.customer = {};
+	}
+
+	basketService.getItems();
+
+	$scope.saveCustomer = function () {
+		localStorageService.set('customer', JSON.stringify($scope.customer));
+	}
+
+	$scope.submitOrder = function () {
+		$scope.submitting = true;
+		var items = [];
+		for (var i = 0; i < basketService.items.length; i++) {
+			items.push({
+				id: basketService.items[i].id,
+				quantity: basketService.items[i].quantity,
+			});
+		}
+
+		$http.post('/order', {
+			items: items,
+			customer: $scope.customer
+		}).success(function () {
+			$scope.submitting = false;
+			window.location = '/confirm';
+		}).error(function () {
+			$scope.submitting = false;
+			$scope.status = 'Order Failed.'
+		})
+	}
+});
+
+app.service('basketService', function ($http, localStorageService) {
+	var self = this;
+
+	try {
+		this.items = JSON.parse(localStorageService.get('basket_items'));
+		if (!this.items) throw Error("does not exist")
+	} catch (e) {
+		this.items = [];
+	}
+
+	this.calculateTotal = function () {
+		self.totalItems = 0;
+		self.items.forEach(function (item) {
+			self.totalItems += item.quantity;
+		});
+	}
+
+	this.calculateTotal();
+
+	this.getItems = function () {
+		this.items.forEach(function (item) {
+			$http.get('/item/' + item.id).success(function (obj) {
+				item.obj = obj;
+			});
+		});
+	}
+
+	this.remove = function (item) {
+		for (var i = 0; i < self.items.length; i++) {
+			if (self.items[i] == item) {
+				self.items.splice(i, 1);
+				self.calculateTotal();
+
+				return;
+			}
+		}
+	}
+
+	this.addItem = function (id) {
+		var found = false;
+
+		for (var i = 0; i < self.items.length; i++) {
+			var item = self.items[i];
+
+			if (item.id != id) continue;
+
+			item.quantity++;
+			found = true;
+			break;
+		}
+
+		if (!found) {
+			self.items.push({
+				id: id,
+				quantity: 1
+			});
+		}
+
+		self.save();
+	}
+
+	this.save = function () {
+		self.calculateTotal();
+		localStorageService.set('basket_items', JSON.stringify(self.items));
+	}
+
+	this.items.forEach(function (item) {
+		if (!item.id) {
+			self.remove(item);
+			self.save();
+			return;
+		}
+	});
+
+	return this;
+});
+
+app.controller('ConfirmCtrl', function ($scope, $rootScope, localStorageService, basketService) {
+	$scope.basket = basketService;
+
+	if (localStorageService.get('customer')) {
+		$scope.customer = JSON.parse(localStorageService.get('customer'));
+	}
+
+	basketService.getItems();
+	$rootScope.hasOrdered = true;
+
+	// localStorageService.set('basket_items', '[]');
+});
--- menu.js
+++ menu.js
@@ -3,110 +3,134 @@ module.exports = [
     name: 'Hot Tapas',
     items: [
       {
+        id: 1,
         name: 'Patates bravas',
         description: 'Diced potates with spicy chilli tomato sause & aioli',
         price: 1.5,
         image: "patates_bravas.jpg",
         attrs: ['v', 'g']
       }, {
+        id: 2,
         name: 'Tortilla de patatas',
         description: 'Spanish omelette made with potato & onion',
         image: "tortilla_de_patatas.jpg",
         attrs: ['v', 'g']
       }, {
+        id: 3,
         name: 'Champinones al ajiool',
         description: 'Sautéed button mushrooms with garlic and parsley',
         image: "champinones.jpg",
         attrs: ['v']
       }, {
+        id: 4,
         name: 'Fritos de queso de cabra a la miel',
         description: 'Deep fried medallions of goats cheese with honey',
         image: "fritos.jpg",
         attrs: ['v']
       }, {
+        id: 5,
         name: 'Chorizos de la casa',
         description: 'Oven baked spicy Spanish sausage',
         image: "chorizo.jpg",
         attrs: ['g']
       }, {
+        id: 6,
         name: 'Lentejas',
         description: 'Slow cooked Spanish brown lentils with chorizo',
         image: "lentejas.jpg",
         attrs: ['g']
       }, {
+        id: 7,
         name: 'Albondigas',
         description: 'Homemade Pork & Beef meatballs in tomato sauce',
         image: "albondingas.jpg"
       }, {
+        id: 8,
         name: 'Pollo con crema y piquillos',
         description: 'Small pieces of chicken breast cooked in cream and piquillo pepper sauce',
         image:"",
         attrs: ['g']
       }, {
+        id: 9,
         name: 'Esparragos a la plancha ',
         description: 'Grilled wild asparagus wrapped with Serrano ham',
         image:"esperagos.jpg",
         attrs: ['g']
       }, {
+        id: 10,
         name: 'Pollo y chorizo con patatas',
         description: 'Small pieces of chicken, chorizo, potatoes sautéed with onion, pepper, white w',
         attrs: ['g']
       }, {
+        id: 11,
         name: 'Milhogas de queso de cabra',
         description: 'Oven baked potato layered with Serrano ham, goats cheese, tomato & bread'
       }, {
+        id: 12,
         name: 'Datiles con bacon',
         description: 'Dates wrapped with bacon and filled with blue cheese. Grilled & served warm',
         image:"datiles.jpg",
         attrs: ['g']
       }, {
+        id: 13,
         name: 'Pinchos de cordero al limón ',
         description: 'Pieces of lamb marinated with lemon juice & cumin, grilled on skewer',
         image: "pinchos.jpg"
       }, {
+        id: 14,
         name: 'Calamares a la romana ',
         description: 'Deep fried squid ',
         image: "calamares.jpg"
       }, {
+        id: 15,
         name: 'Pulpo a la Gallega',
         description: 'Chopped octopus legs served with boiled potatoes topped with olive oil, paprika ',
         image: "pulpo.jpg",
         attrs: ['g']
       }, {
+        id: 16,
         name: 'Mejillones',
         description: 'Fresh mussels with garlic, white wine, parsley & cream',
-        image: "Mejillones"
+        image: "mejillones.jpg"
       }, {
+        id: 17,
         name: 'Gambas a la plancha ',
         description: 'Grilled black tiger prawns served with aioli',
         attrs: ['g'],
         image: "gambas-plancha.jpg"
       }, {
+        id: 18,
         name: 'Gambas al Pil-Pil ',
         description: 'Tiger prawns pan-fried with garlic & chilli oil ',
         attrs: ['g'],
         image: "gampas-pilpil.jpg"
       }, {
+        id: 19,
         name: 'Paella de verduras ',
         description: 'Vegetarian paella of fresh seasonal vegetables',
         attrs: ['v', 'g'],
-        image:"paella-de-verduras.jpg"
+        image: "paella-de-verduras.jpg"
       }, {
+        id: 20,
         name: 'Paella Espanola ',
         description: 'Traditional Paella with chicken & chorizo',
-        image:"paella-espanola.jpg"
+        image: "paella-espanola.jpg"
       }, {
+        id: 21,
         name: 'Paella de marisco',
         description: 'Traditional Mixed Seafood paella ',
         attrs: ['g'],
-        image:"paella-marisco.jpg"
+        image: "paella-marisco.jpg"
       }, {
+        id: 22,
         name: 'Boguerones en adobo',
         description: 'Marinated crispy whitebait'
       }, {
+        id: 23,
         name: 'Bunuelos de bacalao',
         description: 'Light and fluffy cod fritters with garlic, parsley and aioli'
       }, {
+        id: 24,
         name: 'Crocetas de la casa',
         description: 'Ham crocetas'
       }
@@ -115,13 +139,16 @@ module.exports = [
     name: 'Cold Tapas',
     items: [
       {
+        id: 25,
         name: 'Aceitunas',
         description: 'Marinated olives'
       }, {
+        id: 26,
         name: 'Boquerones en vinagre',
         description: 'White marinated anchovies in vinegar and olive oil',
         attrs: ['g']
       }, {
+        id: 27,
         name: 'Pimientos del piquillo',
         description: 'Sweet piquillo peppers in olive oil and garlic dressing.',
         attrs: ['v', 'g']
